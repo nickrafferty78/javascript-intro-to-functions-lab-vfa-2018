41f8214e9b36684e9588f32def5c0c1c951a3565
Nick Rafferty <nickrafferty78@gmail.com>
2018-06-28 18:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string === "I love you, Grandma." ){
-    return 
+    return "I love you, too.";
   }
   if (string.toLowerCase() === string){
     return "I can\'t hear you!";
